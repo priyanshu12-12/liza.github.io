aafdf0873b52d9eb41b36cc429a7691c3aa42393
priyanshu12-12 <sinhapriyanshu81@gmail.com>
2022-07-18 00:05:47 +0530

    quiz

---
+++ index.js
@@ -0,0 +1,39 @@
+function result1() {
+    document.getElementById("answer1").innerHTML = "Right";
+    a = 1;
+}
+function result2() {
+    document.getElementById("answer1").innerHTML = "Wrong";
+}
+function result3() {
+    document.getElementById("answer1").innerHTML = "Wrong";
+}
+function result4() {
+    document.getElementById("answer1").innerHTML = "Wrong";
+}
+function resul11() {
+    document.getElementById("answer2").innerHTML = "Right";
+    a = 2;
+}
+function result12() {
+    document.getElementById("answer2").innerHTML = "Wrong";
+}
+function result13() {
+    document.getElementById("answer2").innerHTML = "Wrong";
+}
+function result14() {
+    document.getElementById("answer2").innerHTML = "Wrong";
+}
+function result21() {
+    document.getElementById("answer3").innerHTML = "Right";
+    a = 3;
+}
+function result22() {
+    document.getElementById("answer3").innerHTML = "Wrong";
+}
+function result23() {
+    document.getElementById("answer3").innerHTML = "Wrong";
+}
+function result24() {
+    document.getElementById("answer3").innerHTML = "Wrong";
+}
